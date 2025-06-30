73686889e9c99c73c092e9d1459ef26d5a0063cc
Matt Adcock <git@matt-is.gay>
2025-07-01 00:28:12 +0100

    ✨ Initial Commit

---
+++ nuxt.config.ts
@@ -0,0 +1,22 @@
+// https://nuxt.com/docs/api/configuration/nuxt-config
+export default defineNuxtConfig({
+  modules: [
+    '@nuxt/content',
+    '@nuxt/eslint',
+    '@nuxt/fonts',
+    '@nuxt/icon',
+    '@nuxt/image',
+  ],
+  devtools: { enabled: true },
+
+  future: {
+    compatibilityVersion: 4,
+  },
+  compatibilityDate: '2025-05-15',
+
+  eslint: {
+    config: {
+      stylistic: true,
+    },
+  },
+})
