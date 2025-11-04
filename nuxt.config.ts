d28a16979cf44f0169c843fa48067b17b9e8a5c6
MattA-Official <43476212+MattA-Official@users.noreply.github.com>
2025-11-04 02:34:04 +0000

    📩 Update dependencies

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -54,10 +54,7 @@ export default defineNuxtConfig({
     },
   },
 
-  future: {
-    compatibilityVersion: 4,
-  },
-  compatibilityDate: '2025-05-15',
+  compatibilityDate: '2025-11-04',
 
   nitro: {
     cloudflare: {
